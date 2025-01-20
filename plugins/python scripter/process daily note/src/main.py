9c9abc9a141e2409303d184329346416a3c1eb6b
Brandt Beal <babeal@outlook.com>
2025-01-20 10:54:41 -0600

    fix(plugin): breaking change in python scripter >=1.1.1

--- plugins/python scripter/process daily note/src/main.py
+++ plugins/python scripter/process daily note/src/main.py
@@ -3,6 +3,8 @@ import sys
 
 """
 Runs the daily note processing script from obsidian_workflow for the selected file
+
+REQUIRES: Python Scripter 1.1.1 or later
 """
 
 SCRIPT_NAME = "OW:DailyNote"
@@ -13,8 +15,8 @@ def log(message):
 
 
 python_script = sys.argv[0]
-file_path = sys.argv[2]
-vault_path = sys.argv[1]
+file_path = sys.argv[1]
+vault_path = sys.argv[2]
 
 if not file_path:
     log("No file path provided.")
@@ -25,10 +27,15 @@ file_path = os.path.abspath(os.path.join(vault_path, file_path))
 log(f"Processing file: {file_path}")
 
 
-def process_daily_note(daily_note_path):
+def run(daily_note_path):
     """Processes a single daily note."""
     try:
         from obsidian_workflow.daily import process_daily_note
     except ImportError:
         print("Error: Could not import process_daily_note from obsidian_workflow.daily")
         sys.exit(1)
+
+    process_daily_note(daily_note_path)
+
+
+run(file_path)
